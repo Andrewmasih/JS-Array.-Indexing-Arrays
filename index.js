388a2ea27a4c8053fa4149d0d6d4edbeb1809ce7
Andrew Masih <armasih@outlook.com>
2021-05-21 16:40:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/** a Array has items, each item has a position, its called an INDEX, and its a number, items in an array start at zero, which in our example zero would be "Ann" */
+
+
+customersOfMyBank = ["Ann", "Barry", "Carl", "Carl", "Edward", "Frank", "Gary", "Harry"];
+
+console.log(customersOfMyBank[2]);
+console.log(customersOfMyBank[3]);
+console.log(customersOfMyBank[4]);
+
+
